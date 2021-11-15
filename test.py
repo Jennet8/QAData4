1536ca8c22538e7ef7b33673ae577ce2709df64c
Sam <sam.i.smith@outlook.com>
2021-11-15 16:01:13 +0000

    some boolean stuff

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 #this is a file to practice git pulling/pushing
 
-var1 = "example text"
-var2 = var1.title()
-print(var2)
+#var1 = "example text"
+#var2 = var1.title()
+#print(var2)
+
+# Numbers
+#number1 = input("Enter whole number: ")
+#number2 = input("Enter decimal number: ")
+
+#integer_number = int(number1)
+#float_number = float(number2)
+#round_number = int(round(float_number))
+
+#print(number1)
+#print(number2)
+#print(round_number)
+
+# Bools
+name = "Pepper"
+age = 3
+bark = True
+tweet = False
+
+print(f"My pet is called {name}. He is {age} years old.")
+print("Statement:", name, "barks.", bark)
+print("Statement:", name, "tweets.", tweet)
